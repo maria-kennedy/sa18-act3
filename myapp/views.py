91ff17ab9bf56f3bc584ce031257ef1c036d4256
Maria Kennedy <mariakingsbury1@gmail.com>
2024-05-01 17:19:00 -0500

    created index and show pages

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'myapp/index.html')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
-def show(request):
-    return render(request,'myapp/show.html')
+def show(request,id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'show.html', {'product': product})
